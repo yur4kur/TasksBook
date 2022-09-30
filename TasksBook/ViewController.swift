2c568445bc55db6f41714f352c79d762f55b84de
Iurii Kurinnoi <yur4kur@icloud.com>
2022-09-30 16:18:28 +0400

    Initial Commit

---
+++ TasksBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TasksBook
+//
+//  Created by Юрий Куринной on 30.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
